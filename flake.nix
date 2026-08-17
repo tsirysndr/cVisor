1c64181264c8f071b4ddf1ffc0460434a4dd6c68
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-08-17 13:31:26 +0300

    chore(release): bump cvisor to 0.1.3
    Workspace crates, the nix flake's web derivations, and the npm CLI
    packages (including the platform pins) move together.

--- flake.nix
+++ flake.nix
@@ -51,7 +51,7 @@
 
         webNodeModules = pkgs.stdenv.mkDerivation {
           pname = "cvisor-web-node-modules";
-          version = "0.1.2";
+          version = "0.1.3";
           src = webDepsSrc;
           nativeBuildInputs = [ pkgs.bun ];
           dontConfigure = true;
@@ -84,7 +84,7 @@
 
         web = pkgs.stdenv.mkDerivation {
           pname = "cvisor-web";
-          version = "0.1.2";
+          version = "0.1.3";
           src = ./ui;
           nativeBuildInputs = [ pkgs.bun pkgs.nodejs ];
           configurePhase = ''
